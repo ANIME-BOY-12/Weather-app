8a314667be24f24c01fc53308f715b83ca570d0e
ANIME-BOY-12 <avinashtiwarik12@gmail.com>
2024-11-16 15:41:41 +0530

    version1.0

---
+++ script.js
@@ -0,0 +1,43 @@
+// http://api.weatherapi.com/v1/current.json?key=945e8600376a42babd740639240110&q=NOIDA&aqi=no
+
+let target ="lucknow"
+
+searchButton.addEventListener('click',searchForLocation)
+
+const fetchdata = async(targetLocation)=>{
+    let url = `http://api.weatherapi.com/v1/current.json?key=945e8600376a42babd740639240110&q=${targetLocation}&aqi=no`
+
+    const res = await fetch(url)
+    // console.log(res);
+    const data = await res.json()
+
+    console.log(data);
+
+    let icon = document.querySelector(".weather-icon")
+    icon.src = data.current.condition.icon
+    
+    let city = document.querySelector(".city")
+    city.textContent = data.location.name
+
+    let temp = document.querySelector(".temp")
+    temp.textContent = data.current.temp_c
+
+
+    let humidity = document.querySelector(".p1")
+    humidity.textContent = `Humidity : ${data.current.humidity}` 
+
+
+    let wind = document.querySelector(".p2")
+    wind.textContent = `Wind-Speed : ${data.current.humidity}` 
+}
+
+function searchForLocation(e){
+    e.preventDefault()
+
+    target = document.querySelector(".searchBox").value
+
+    // console.log(target)
+    fetchdata(target)
+}
+
+fetchdata(target)
